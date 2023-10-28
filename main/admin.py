2924f50c027d2b7fd347ec09b9b50ef519760da2
Rasulbek <rasulbekxoldorov8@gmail.com>
2023-10-28 19:06:37 +0500

    start

---
+++ main/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from main.models import Home
+
+@admin.register(Home)
+class Home(admin.ModelAdmin):
+    list_display = ('title',)
+    list_display_links = ('title',)
---
+++ main/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Home(models.Model):
+    banner = models.ImageField(upload_to='media/banner/%Y/%m/%d')
+    title = models.CharField(max_length=255, blank=True)
+
+    def __str__(self) -> str:
+        return self.title
+    
+class Meta:
+    verbose_name = 'Home'
+    verbose_name_plural = 'Homes'
